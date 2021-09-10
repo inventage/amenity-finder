f920764032a0fd14e783ea88b7884c2e0457ec5b
Peter Siska <psiska@inventage.com>
2021-09-10 14:21:04 +0200

    Enable SPA routing

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -10,12 +10,12 @@ export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   nodeResolve: {
     exportConditions: ['browser', 'development'],
   },
-  
+
   /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
   // esbuildTarget: 'auto'
 
   /** Set appIndex to enable SPA routing */
-  // appIndex: 'demo/index.html',
+  appIndex: 'index.html',
 
   plugins: [
     /** Use Hot Module Replacement by uncommenting. Requires @open-wc/dev-server-hmr plugin */
