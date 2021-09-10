6ef92c27458d1aa151fde785ff4bcee26e93e171
Peter Siska <psiska@inventage.com>
2021-09-10 13:13:57 +0200

    Add mwc components

--- src/AmenityFinder.js
+++ src/AmenityFinder.js
@@ -1,87 +1,31 @@
 import { LitElement, html, css } from 'lit';
+import '@material/mwc-drawer';
+import '@material/mwc-top-app-bar';
 
 export class AmenityFinder extends LitElement {
-  static get properties() {
-    return {
-      title: { type: String },
-    };
-  }
-
   static get styles() {
     return css`
       :host {
         min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-start;
-        font-size: calc(10px + 2vmin);
-        color: #1a2b42;
-        max-width: 960px;
-        margin: 0 auto;
-        text-align: center;
-        background-color: var(--amenity-finder-background-color);
-      }
-
-      main {
-        flex-grow: 1;
-      }
-
-      .logo {
-        margin-top: 36px;
-        animation: app-logo-spin infinite 20s linear;
-      }
-
-      @keyframes app-logo-spin {
-        from {
-          transform: rotate(0deg);
-        }
-        to {
-          transform: rotate(360deg);
-        }
-      }
-
-      .app-footer {
-        font-size: calc(12px + 0.5vmin);
-        align-items: center;
-      }
-
-      .app-footer a {
-        margin-left: 5px;
       }
     `;
   }
 
-  constructor() {
-    super();
-    this.title = 'My app';
-  }
-
   render() {
     return html`
-      <main>
-        <h1>${this.title}</h1>
-
-        <p>Edit <code>src/AmenityFinder.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+      <mwc-drawer>
+        <div>
+          <p>Drawer Content!</p>
+        </div>
+        <div slot="appContent">
+          <mwc-top-app-bar>
+            <div slot="title">Title</div>
+          </mwc-top-app-bar>
+          <div>
+            <p>Main Content!</p>
+          </div>
+        </div>
+      </mwc-drawer>
     `;
   }
 }
