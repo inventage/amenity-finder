a68e4e20f5a34492abc8572792b95f3d28400e94
Peter Siska <psiska@inventage.com>
2021-09-10 15:03:03 +0200

    Add default search values

--- src/views/SearchView.js
+++ src/views/SearchView.js
@@ -1,6 +1,7 @@
 import { html, LitElement } from 'lit';
 import '@material/mwc-button';
 import '@material/mwc-textfield';
+import '@inventage/leaflet-map';
 
 export class SearchView extends LitElement {
   static get properties() {
@@ -11,22 +12,26 @@ export class SearchView extends LitElement {
     };
   }
 
+  constructor() {
+    super();
+
+    this.latitude = '47.3902';
+    this.longitude = '8.5158';
+    this.radius = 1000;
+  }
+
   render() {
     return html`
       <h1>Search</h1>
 
-      <mwc-textfield label="Latitude" @keyup="${e => (this.latitude = e.target.value)}"></mwc-textfield>
-      <mwc-textfield label="Longitude" @keyup="${e => (this.longitude = e.target.value)}"></mwc-textfield>
-      <mwc-textfield label="Radius (m)" @keyup="${e => (this.radius = e.target.value)}"></mwc-textfield>
+      <mwc-textfield label="Latitude" .value="${this.latitude}" @keyup="${e => (this.latitude = e.target.value)}"></mwc-textfield>
+      <mwc-textfield label="Longitude" .value="${this.longitude}" @keyup="${e => (this.longitude = e.target.value)}"></mwc-textfield>
+      <mwc-textfield label="Radius (m)" .value="${this.radius}" @keyup="${e => (this.radius = e.target.value)}"></mwc-textfield>
 
       <mwc-button outlined label="Locate Me" icon="my_location"></mwc-button>
       <mwc-button raised label="Search"></mwc-button>
 
-      <p>
-        Latitude: ${this.latitude}<br />
-        Longitude: ${this.longitude}<br />
-        Radius: ${this.radius}
-      </p>
+      <leaflet-map .latitude="${this.latitude}" .longitude="${this.longitude}" .radius="${this.radius}"></leaflet-map>
     `;
   }
 }
