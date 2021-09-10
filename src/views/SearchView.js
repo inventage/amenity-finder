e93f573eacc55a94710bfd27d30296151dff178b
Peter Siska <psiska@inventage.com>
2021-09-10 14:59:31 +0200

    Add search form

--- src/views/SearchView.js
+++ src/views/SearchView.js
@@ -1,8 +1,33 @@
 import { html, LitElement } from 'lit';
+import '@material/mwc-button';
+import '@material/mwc-textfield';
 
 export class SearchView extends LitElement {
+  static get properties() {
+    return {
+      latitude: { type: String },
+      longitude: { type: String },
+      radius: { type: Number },
+    };
+  }
+
   render() {
-    return html`Search…`;
+    return html`
+      <h1>Search</h1>
+
+      <mwc-textfield label="Latitude" @keyup="${e => (this.latitude = e.target.value)}"></mwc-textfield>
+      <mwc-textfield label="Longitude" @keyup="${e => (this.longitude = e.target.value)}"></mwc-textfield>
+      <mwc-textfield label="Radius (m)" @keyup="${e => (this.radius = e.target.value)}"></mwc-textfield>
+
+      <mwc-button outlined label="Locate Me" icon="my_location"></mwc-button>
+      <mwc-button raised label="Search"></mwc-button>
+
+      <p>
+        Latitude: ${this.latitude}<br />
+        Longitude: ${this.longitude}<br />
+        Radius: ${this.radius}
+      </p>
+    `;
   }
 }
 
