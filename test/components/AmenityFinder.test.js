cf06b027eacea0c22cd03a74af75558de831c113
Peter Siska <psiska@inventage.com>
2021-09-14 15:03:16 +0200

    Add test for amenity-finder

---
+++ test/components/AmenityFinder.test.js
@@ -0,0 +1,30 @@
+import { fixture, expect } from '@open-wc/testing';
+import { html } from 'lit/static-html.js';
+
+import '../../src/AmenityFinder.js';
+
+describe('<amenity-finder>', () => {
+  describe('Structure', () => {
+    it('has a rendered attribute', async () => {
+      const el = await fixture(html`<amenity-finder></amenity-finder>`);
+      expect(el).to.have.attribute('rendered');
+    });
+  });
+
+  describe('Accessibility', () => {
+    it('is displayed by default', async () => {
+      const el = await fixture(html`<amenity-finder></amenity-finder>`);
+      expect(el).to.be.displayed;
+    });
+
+    it.skip('is accessible', async () => {
+      const el = await fixture(html`<amenity-finder></amenity-finder>`);
+      await expect(el).to.be.accessible();
+    });
+
+    it('is hidden when attribute hidden is true', async () => {
+      const el = await fixture(html`<amenity-finder hidden></amenity-finder>`);
+      expect(el).not.to.be.displayed;
+    });
+  });
+});
